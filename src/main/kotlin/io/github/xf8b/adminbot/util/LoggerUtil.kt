274996ca5cce9626835a4b7fce6178dc0ffadea8
xf8b <yesman58194@tutanota.com>
2020-08-27 14:05:13 -0700

    credit for LoggerUtil.kt

--- src/main/kotlin/io/github/xf8b/adminbot/util/LoggerUtil.kt
+++ src/main/kotlin/io/github/xf8b/adminbot/util/LoggerUtil.kt
@@ -3,4 +3,5 @@ package io.github.xf8b.adminbot.util
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+//credit: https://www.reddit.com/r/Kotlin/comments/8gbiul/slf4j_loggers_in_3_ways/
 inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
